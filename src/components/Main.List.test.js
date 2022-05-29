ea5d41e59a73962675928b50541242be7987cf8d
Twalinzor <twalin.rift@gmail.com>
2022-05-29 19:36:03 +0200

    Update

--- src/components/Main.List.test.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "@testing-library/jest-dom/extend-expect"
-import { render } from "@testing-library/react";
-import MainList from "./MainList";
-
-test("renders content", () => {
-   
-})
